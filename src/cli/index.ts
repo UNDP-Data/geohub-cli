680770692da8ef4c1584537b0cbd37c2772bcf62
Jin Igarashi <igarashi.jin@water-gis.com>
2022-11-18 21:06:07 +0000

    added STAC command

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,6 +2,7 @@ import { Command } from 'commander';
 import azblob from './azblob';
 import martin from './martin';
 import pgtileserv from './pgtileserv';
+import stac from './stac';
 
 const program = new Command();
 const version = require('../../package.json').version;
@@ -9,6 +10,7 @@ program
 	.version(version, '-v, --version', 'output the version number')
 	.addCommand(azblob)
 	.addCommand(martin)
-	.addCommand(pgtileserv);
+	.addCommand(pgtileserv)
+	.addCommand(stac);
 
 program.parse(process.argv);
